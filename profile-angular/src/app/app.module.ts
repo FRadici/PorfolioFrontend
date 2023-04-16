7562960bc715e0b1985623df99867e84e7413607
FRadici <flavia.radici@mi.unc.edu.ar>
2023-04-16 16:33:08 -0300

    Angular-componente curriculum

--- profile-angular/src/app/app.module.ts
+++ profile-angular/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { SobremiComponent } from './componentes/sobremi/sobremi.component';
 import { SkillsComponent } from './componentes/skills/skills.component';
+import { CurriculumComponent } from './componentes/curriculum/curriculum.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     SobremiComponent,
-    SkillsComponent
+    SkillsComponent,
+    CurriculumComponent
   ],
   imports: [
     BrowserModule
