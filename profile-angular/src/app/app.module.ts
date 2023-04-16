7ae9bed848917b43fd0dcf4c6e7d938936637c6e
FRadici <flavia.radici@mi.unc.edu.ar>
2023-04-16 16:28:07 -0300

    Angular-componente sobremi

--- profile-angular/src/app/app.module.ts
+++ profile-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    SobremiComponent
   ],
   imports: [
     BrowserModule
