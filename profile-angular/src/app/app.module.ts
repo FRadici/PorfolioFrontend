6d33611c90b618459fcb5defc7eaf3d69166efaa
FRadici <flavia.radici@mi.unc.edu.ar>
2023-04-16 16:30:32 -0300

    Angular-componente skills

--- profile-angular/src/app/app.module.ts
+++ profile-angular/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { SobremiComponent } from './componentes/sobremi/sobremi.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    SobremiComponent
+    SobremiComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule
