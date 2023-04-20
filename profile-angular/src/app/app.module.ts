9232f3895c51cd2cf365db523cd3451eee4261ce
FRadici <flavia.radici@mi.unc.edu.ar>
2023-04-20 05:45:21 -0300

    componente - contacto

--- profile-angular/src/app/app.module.ts
+++ profile-angular/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { SobremiComponent } from './componentes/sobremi/sobremi.component';
 import { SkillsComponent } from './componentes/skills/skills.component';
 import { CurriculumComponent } from './componentes/curriculum/curriculum.component';
 import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { PortfolioComponent } from './componentes/portfolio/portfolio.component'
     SobremiComponent,
     SkillsComponent,
     CurriculumComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
