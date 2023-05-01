b441877cfbd020cfe76b91444b1e864ef200b031
FRadici <flavia.radici@mi.unc.edu.ar>
2023-05-01 09:17:31 -0300

    Componentes

---
+++ b.ProfileNuevoAngular/src/app/@components/curriculum/curriculum.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CurriculumComponent } from './curriculum.component';
+
+
+
+@NgModule({
+  declarations: [
+    CurriculumComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CurriculumModule { }
