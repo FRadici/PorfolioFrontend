615dd2b7a3aa1b9070b675353c6eadd74b0b38f2
FRadici <flavia.radici@mi.unc.edu.ar>
2023-05-06 15:27:18 -0300

    form

--- b.ProfileNuevoAngular/src/app/@components/usuario/usuario.component.ts
+++ b.ProfileNuevoAngular/src/app/@components/usuario/usuario.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
   styleUrls: ['./usuario.component.css']
 })
-export class UsuarioComponent {
-
+export class UsuarioComponent implements OnInit { 
+  form:FormGroup;
+ constructor(private formBuilder:FormBuilder){
+  this.form=this.formBuilder.group(
+    {
+      CorreoElectronico:['',[Validators.required,Validators.CorreoElectronico]],
+      contraseña:['', [Validators.required,Validators.minLength(8)]],
+      deviceInfo:this.formBuilder.group({
+      deviceId:[ "17867868768"],
+      deviceType:["DEVICE_TYPE_ANDROID"],
+      notificationToken:["67657575eececc34"]
+      })
+    }
+  )
+}
+ngOnInit():void { 
+}
 }
--- b.ProfileNuevoAngular/src/app/app.module.ts
+++ b.ProfileNuevoAngular/src/app/app.module.ts
@@ -13,28 +13,39 @@ import { PorfolioModule } from './@components/porfolio/porfolio.module';
 import { SkillsModule } from './@components/skills/skills.module';
 import { FooterModule } from './@components/footer/footer.module';
 import { UsuarioModule } from './@components/usuario/usuario.module';
+import { NavbarComponent } from './@components/navbar/navbar.component';
+import { HeaderComponent } from './@components/header/header.component';
+import { PorfolioComponent } from './@components/porfolio/porfolio.component';
+import { AboutComponent } from './@components/about/about.component';
+import { ContactComponent } from './@components/contact/contact.component';
+import { FooterComponent } from './@components/footer/footer.component';
+import { CopyrightComponent } from './@components/copyright/copyright.component';
+import { CurriculumComponent } from './@components/curriculum/curriculum.component';
+import { SkillsComponent } from './@components/skills/skills.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    PorfolioComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    CopyrightComponent,
+    CurriculumComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AboutModule,
-    ContactModule,
-    CopyrightModule,
-    CurriculumModule,
-    HeaderModule,
-    NavbarModule,
-    PorfolioModule,
-    SkillsModule,
-    FooterModule,
-    UsuarioModule,
+    ReactiveFormsModule,
    
-  
+    
       ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
